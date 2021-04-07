bf53ad9d7ed3482aa241330d559dc9402b461c9e
Giuseppe <festagiuseppe89@gmail.com>
2021-04-07 17:37:22 +0200

    correzioni varie,milestone 3 e layout quasi completo

--- main.js
+++ main.js
@@ -1,58 +1,71 @@
-const apiUrlPrincipale = 'https://api.themoviedb.org/3/';
-const apiMyKey = '28fd8bc3a7d1855c34222ce81dae240e';
-
+const api_url_base = 'https://api.themoviedb.org/3/';
+const api_key = '28fd8bc3a7d1855c34222ce81dae240e';
+const url_base_locandina = 'https://image.tmdb.org/t/p/';
+const dimensione_locandina = 'w342';
+const locandina_default = 'img/netflix.png';
 
 var app = new Vue({
     el: '#app',
     data: {
-        trovati: [],
-        ricerca: '',
-        titolo: '',
-        ricercaInCorso: false,
-        
+        risultati: [],
+        testo_ricerca: '',
+        testo_titolo: '',
+        ricerca_in_corso: false,
+        bandiere_disponibili: ['it', 'en', 'ro']
     },
     methods: {
-        ricercaFilm() {
+        ricerca() {
 
-            if(this.ricerca.trim() != '') {
-                this.ricercaInCorso = true;
+            if(this.testo_ricerca.trim() != '') {
+
+                this.ricerca_in_corso = true;
 
                 //azzero container input e risultati
-                this.trovati = [];
-                let textUser = this.textSearch;
-                this.textSearch = '';
-
+                this.risultati = [];
+                let testo_utente = this.testo_ricerca;
+                this.testo_ricerca = '';
                 //inserisco il testo cercato nel titolo
-                this.titolo = textUser;
+                this.testo_titolo = testo_utente;
 
                 let parameters = {
                     params: {
-                        apiMyKey: apiMyKey,
-                        query: textUser
+                        api_key: api_key,
+                        query: testo_utente
                     }
                 };
 
                 //richiamo ajax a TMBD e trovo films
-                axios
-                .get(apiUrlPrincipale + 'search/movie', parameters)
-                .then((risp) => {
-                    this.trovati = this.trovati.concat(risp.data.results);
-                    //console.log(risp.data.results);
-                    this.ricercaInCorso = false;
+                axios.get(api_url_base + 'search/movie', parameters)
+                    .then((risp) => {
+                    console.log(risp.data.results);
+                    this.risultati = this.risultati.concat(risp.data.results);
+                    this.ricerca_in_corso = false;
                 });
-                //richiamo ajax a TMBD e trovo le serie TV
-                axios
-                .get(apiUrlPrincipale + 'search/tv', parameters)
-                .then((risp) => {
-                    this.trovati = this.trovati.concat(risp.data.results);
-                    this.ricercaInCorso = false;
-                })
+
+                // richiamo ajax a TMBD e trovo le serie TV
+                axios.get(api_url_base + 'search/tv', parameters)
+                    .then((risp) => {
+                    // console.log(risp.data.results);
+                    this.risultati = this.risultati.concat(risp.data.results);
+                    this.ricerca_in_corso = false;
+                });
+
             }
-            
         },
-        get_numero_stelle(voto) {
+        //voto stars
+        getStars(voto) {
             return Math.ceil(voto / 2);
         },
-        
+        //flags + languages
+        getFlags(lingua) {
+            return 'flags/' + lingua + '.png';
+        },
+        //poster film + dimensione + path
+        getPoster(poster_path) {
+            if(poster_path) {
+                return url_base_locandina + dimensione_locandina + poster_path;
+            }
+            return locandina_default;
+        }
     },
 });
